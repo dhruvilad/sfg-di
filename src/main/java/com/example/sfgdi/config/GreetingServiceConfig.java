097ea59fa9cf8a68f1cf586ec4aea6e38970ee17
dhruvilad <dhruvi.lad2010@gmail.com>
2023-01-04 16:23:07 +0530

    Java COnfiguration added- removed @Service annotation from service class and used them via java configuration file

---
+++ src/main/java/com/example/sfgdi/config/GreetingServiceConfig.java
@@ -0,0 +1,20 @@
+package com.example.sfgdi.config;
+
+import com.example.sfgdi.services.ConstructorInjectedGreetingService;
+import com.example.sfgdi.services.PropertyInjectedGreetingService;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class GreetingServiceConfig {
+
+    @Bean
+    ConstructorInjectedGreetingService constructorInjectedGreetingService(){
+        return new ConstructorInjectedGreetingService();
+    }
+
+    @Bean
+    PropertyInjectedGreetingService propertyInjectedGreetingService(){
+        return new PropertyInjectedGreetingService();
+    }
+}
