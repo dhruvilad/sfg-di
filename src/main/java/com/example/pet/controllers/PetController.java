6901bc539c1a7c969f604db037991529e874b6e9
dhruvilad <dhruvi.lad2010@gmail.com>
2023-01-04 16:18:29 +0530

    component scan changes

---
+++ src/main/java/com/example/pet/controllers/PetController.java
@@ -0,0 +1,22 @@
+package com.example.pet.controllers;
+
+import com.example.pet.services.PetService;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Controller;
+
+/**
+ * Created by jt on 12/28/19.
+ */
+@Controller
+public class PetController {
+
+    private final PetService petService;
+
+    public PetController(@Qualifier("pet") PetService petService) {
+        this.petService = petService;
+    }
+
+    public String whichPetIsTheBest(){
+        return petService.getPetType();
+    }
+}
--- src/main/java/com/example/sfgdi/SfgDiApplication.java
+++ src/main/java/com/example/sfgdi/SfgDiApplication.java
@@ -1,18 +1,26 @@
 package com.example.sfgdi;
 
+import com.example.pet.controllers.PetController;
 import com.example.sfgdi.controllers.*;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"com.example.sfgdi","com.example.pet"})
 public class SfgDiApplication {
 
 	public static void main(String[] args) {
 		ApplicationContext ctx=SpringApplication.run(SfgDiApplication.class, args);
 
-		I18nController i18nController=(I18nController) ctx.getBean("i18nController");
+		PetController petController = ctx.getBean("petController", PetController.class);
+		System.out.println("--- The Best Pet is ---");
+		System.out.println(petController.whichPetIsTheBest());
 
+
+
+		I18nController i18nController=(I18nController) ctx.getBean("i18nController");
 		System.out.println("-------------- i18nController");
 		System.out.println(i18nController.sayHello());
 
